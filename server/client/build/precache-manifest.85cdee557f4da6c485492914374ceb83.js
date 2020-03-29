0e8c43513fe656b33c0bfea7d8f4d5d2ee89e6aa
WhiteRiceWill <wmchristman123@gmail.com>
2020-03-29 02:24:50 -0400

    test

---
+++ server/client/build/precache-manifest.85cdee557f4da6c485492914374ceb83.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6839804ad535b6a2f841",
+    "url": "/static/css/main.81b622f7.chunk.css"
+  },
+  {
+    "revision": "6839804ad535b6a2f841",
+    "url": "/static/js/main.6839804a.chunk.js"
+  },
+  {
+    "revision": "fcfed040ec0d418dd048",
+    "url": "/static/js/1.fcfed040.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "81ba4d718cdc6226e6c1f62c513c4833",
+    "url": "/static/media/stitchCoinLogo.81ba4d71.png"
+  },
+  {
+    "revision": "b619bbd0f06d7f1631441843e831673a",
+    "url": "/static/media/background.b619bbd0.jpg"
+  },
+  {
+    "revision": "54faeb5143abb90fe37f8607a6883dd7",
+    "url": "/static/media/GoodTimes-Regular.54faeb51.woff"
+  },
+  {
+    "revision": "24bd87b6526d946d6a081e75b4f3fbe9",
+    "url": "/index.html"
+  }
+];
